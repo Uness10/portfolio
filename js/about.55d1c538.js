90efd0e3b0c10d5af8f376cc90f116ca07f4c67e
Uness10 <younessanouar57@gmail.com>
2025-02-15 01:49:45 +0100

    Updates

--- js/about.55d1c538.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkportfolio"]=self["webpackChunkportfolio"]||[]).push([[594],{709:function(n,t,u){u.r(t),u.d(t,{default:function(){return i}});var o=u(641);const r={class:"about"};function a(n,t){return(0,o.uX)(),(0,o.CE)("div",r,t[0]||(t[0]=[(0,o.Lk)("h1",null,"This is an about page",-1)]))}var e=u(262);const s={},c=(0,e.A)(s,[["render",a]]);var i=c}}]);
-//# sourceMappingURL=about.55d1c538.js.map
